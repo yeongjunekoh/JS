43c3eb6e5cffd7de7ea528aa38301d335e9dfbd4
yeongjunekoh <rhdudwns0@naver.com>
2020-10-05 14:36:14 +0900

    practice

--- example.js
+++ example.js
@@ -1,1 +1,8 @@
-document.write("환영합니다.");
+var price1 = 3000;
+var price2 = 5000;
+var price3 = 3000;
+var sum = price1 + price2 + price3;
+var result;
+var result = (sum) > 10000 ? "원 초과하셨습니다." : "돈 관리 잘했어요!";
+var over = sum - 10000;
+document.write(over + result);
